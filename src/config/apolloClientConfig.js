78f097a4baa4f7a51e47de83e24665f95361e5f2
agustinantonucci <agustinantonucci@gmail.com>
2022-11-02 11:49:01 -0300

    cambio de uri

--- src/config/apolloClientConfig.js
+++ src/config/apolloClientConfig.js
@@ -13,7 +13,7 @@ const Client = new ApolloClient({
 	connectToDevTools: true,
 	cache: new InMemoryCache(),
 	link: new HttpLink({
-		uri: `http://10.0.0.153:4002/graphql`
+		uri: `https://75ea-152-169-48-128.sa.ngrok.io/graphql`
 		//uri: process.env.REACT_APP_APOLLO,
 		//uri: URL,
 		// uri: `http://beeapp.binamics.com.ar:4002`
